735006f79125dcb4eb25006dbb1dba604e3abeef
Kristiana Blake <kristiana.blake@gmail.com>
2020-12-22 12:54:56 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OMDbApiNet;
+using OMDbApiNet.Model;
+
+namespace OmdbAPiPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OmdbClient client = new OmdbClient("942af2e");
+            Item userSearch = client.GetItemByTitle("Batman");
+            Console.WriteLine(userSearch.Title);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
